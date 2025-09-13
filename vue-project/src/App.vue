1066220556dd00dd2c011f95a2ac9dfe07fd4fe2
Ta Chi Nguyen <tcnguyen93@gmail.com>
2025-09-13 15:55:15 +0700

    up

---
+++ vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('Hello World!')
+const header = ref('Shoping list')
+const products = ref([{id:1,label:"product1"},{id:2, label:"product2"},{id:3,label: "product3"}])
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <h2>{{header}}</h2>
+<ul>
+  <li v-for="product in products" :key="product.id">{{product.label}}</li>
+</ul>
+  <input v-model="msg" />
+</template>
